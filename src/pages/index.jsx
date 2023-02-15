8c469437382fab78c521adc2d4fc69b8794fd109
Raviteja Manepalli <ravitejamanepalli@Ravitejas-MacBook-Air.local>
2023-02-15 13:51:47 +0530

    added changes from taxpal to growth farm

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,10 +14,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TaxPal - Accounting made simple for small businesses</title>
+        <title>GrowthFarm - IT Servicing</title>
         <meta
           name="description"
-          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+          content="Helping you in delvering production grade IT services"
         />
       </Head>
       <Header />
